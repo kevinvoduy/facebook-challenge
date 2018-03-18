ad17ee1e11506e89e9bc39ff876c4a39bce158fb
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-17 21:53:50 -0700

    [search] completed logic for seach - need to optimize sort

--- app/controllers/fetcher_controller.rb
+++ app/controllers/fetcher_controller.rb
@@ -1,4 +1,28 @@
 class FetcherController < ApplicationController
-  def index
+  require 'net/http'
+  require 'json'
+
+  def get
+  end
+
+  def show
+    token = params[:token]
+    k = params[:k]
+    @postlimit = params[:n]
+    query = params[:q]
+    @query = params[:q]
+
+    uri = URI.parse("https://graph.facebook.com/v2.12/" + query + "/posts?fields=message%2Cshares&limit=" + k + "&access_token=" + token)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    res = http.request(req)
+
+    array = JSON.parse res.body
+
+    sortedArray = {
+      :data => array['data'].sort_by { |hash| -hash['shares']['count'] }
+    }
+    @lib = sortedArray
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'fetcher/index'
-
-  resources :data
+  resources :fetcher
 
   root 'fetcher#index'
 end
